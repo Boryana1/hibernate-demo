9d958db21cb5b478abdc8a6cca22174eb0cbd385
Boryana1 <boryana.dimova1@gmailcom>
2023-01-25 11:16:39 -0600

    Added getAllUsers() and getUserById methods

--- src/main/java/com/bd/HibernateDemo/services/LocationService.java
+++ src/main/java/com/bd/HibernateDemo/services/LocationService.java
@@ -1,6 +1,24 @@
 package com.bd.HibernateDemo.services;
+import com.bd.HibernateDemo.models.Location;
+import com.bd.HibernateDemo.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LocationService {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public List<Location> getAllLocations() {
+        return (List<Location>) locationRepository.findAll();
+    }
+
+    public Optional<Location> getLocationById(Integer id) {
+        return locationRepository.findById(id);
+    }
+
 }
