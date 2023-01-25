7b4774bb43bc1eb193482bd5e88f03816f66e673
Boryana1 <boryana.dimova1@gmailcom>
2023-01-25 16:13:00 -0600

    Added findByUserId

--- src/main/java/com/bd/HibernateDemo/repositories/PostRepository.java
+++ src/main/java/com/bd/HibernateDemo/repositories/PostRepository.java
@@ -4,6 +4,11 @@ import com.bd.HibernateDemo.models.Post;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post, Integer> {
+
+    List<Post> findByUserId(Integer id);
+
 }
