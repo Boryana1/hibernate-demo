5b390b3819e0df7ba1748f501cdf536a4976c675
Boryana1 <boryana.dimova1@gmailcom>
2023-01-25 11:07:35 -0600

    Added mapping for list of Users by location id

--- src/main/java/com/bd/HibernateDemo/controllers/LocationController.java
+++ src/main/java/com/bd/HibernateDemo/controllers/LocationController.java
@@ -1,6 +1,42 @@
 package com.bd.HibernateDemo.controllers;
+import com.bd.HibernateDemo.models.Location;
+import com.bd.HibernateDemo.models.User;
+import com.bd.HibernateDemo.repositories.LocationRepository;
+import com.bd.HibernateDemo.services.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LocationController {
+
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping("/locations")
+    public List<Location> getAllLocations() {
+        return locationService.getAllLocations();
+    }
+
+    @GetMapping("/locations/{id}")
+    public Optional<Location> getLocationById(@PathVariable Integer id){
+        return locationService.getLocationById(id);
+    }
+
+    @GetMapping("/location/{id}/users")
+    public List<User> getUsersByLocation(@PathVariable Integer id) {
+        Optional<Location> location = locationService.getLocationById(id);
+
+        if(location.isPresent()) {
+            Location newLocation = location.get();
+            return newLocation.getUsers();
+        }else{
+            return null;
+        }
+    }
+
 }
