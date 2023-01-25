e325b507745860d4052ed23cf19d1ae5e521c8ce
Boryana1 <boryana.dimova1@gmailcom>
2023-01-25 11:14:12 -0600

    Added mapping for getting all the users and getting the users by id

--- src/main/java/com/bd/HibernateDemo/controllers/UserController.java
+++ src/main/java/com/bd/HibernateDemo/controllers/UserController.java
@@ -1,6 +1,27 @@
 package com.bd.HibernateDemo.controllers;
+import com.bd.HibernateDemo.models.User;
+import com.bd.HibernateDemo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> getUserById(@PathVariable Integer id) {
+        return userService.getUserById(id);
+    }
 }
