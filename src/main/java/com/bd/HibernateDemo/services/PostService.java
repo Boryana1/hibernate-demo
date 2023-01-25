a97e38f53e33a727b426d71f74d8c321b10f2d78
Boryana1 <boryana.dimova1@gmailcom>
2023-01-25 11:17:12 -0600

    Added getAllPosts and getPostById methods

--- src/main/java/com/bd/HibernateDemo/services/PostService.java
+++ src/main/java/com/bd/HibernateDemo/services/PostService.java
@@ -1,6 +1,24 @@
 package com.bd.HibernateDemo.services;
+import com.bd.HibernateDemo.models.Post;
+import com.bd.HibernateDemo.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> getAllPosts() {
+        return (List<Post>) postRepository.findAll();
+    }
+
+    public Optional<Post> getPostById(Integer id) {
+        return postRepository.findById(id);
+    }
+
 }
