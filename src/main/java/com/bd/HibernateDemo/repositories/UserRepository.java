0a964f24058cacff9b7d098cf0e35ad6671e3389
Boryana1 <boryana.dimova1@gmailcom>
2023-01-25 16:30:41 -0600

    Added findByLocationId

--- src/main/java/com/bd/HibernateDemo/repositories/UserRepository.java
+++ src/main/java/com/bd/HibernateDemo/repositories/UserRepository.java
@@ -4,8 +4,13 @@ import com.bd.HibernateDemo.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    List<User> findByLocationId(Integer id);
+
 }
 
 
