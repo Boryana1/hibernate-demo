5f625a6474e8152c89e2591cb20b3a7682e46348
Boryana1 <boryana.dimova1@gmailcom>
2023-01-25 11:17:53 -0600

    Added getAllUsers and getUserById methods

--- src/main/java/com/bd/HibernateDemo/services/UserService.java
+++ src/main/java/com/bd/HibernateDemo/services/UserService.java
@@ -1,6 +1,24 @@
 package com.bd.HibernateDemo.services;
+import com.bd.HibernateDemo.models.User;
+import com.bd.HibernateDemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+
 }
